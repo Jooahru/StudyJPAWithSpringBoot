97ceff3a39c7148561e723c8f8b18caa30b0f592
WILL <dndlf333@naver.com>
2023-06-12 23:59:27 +0900

    [기능 추가] Entity 생성1

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
