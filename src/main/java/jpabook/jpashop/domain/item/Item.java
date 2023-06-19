9f6590bf850f00f21572ce99d032e83d17a38f6e
WILL <dndlf333@naver.com>
2023-06-19 23:36:17 +0900

    [기능 개선] 비즈니스 로직 추가
     -재고 추가/감소 로직 추가
     -Exception 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
@@ -11,7 +12,9 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
+
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -33,5 +36,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items", fetch = FetchType.LAZY)
     private List<Category> categories = new ArrayList<>();
+    
+    //==비즈니스 로직==//
+    /*
+     * stock 증가
+     * */
+    public void addStock(int quantity) {
+    	this.stockQuantity += quantity;
+    }
+    
+    /*
+     * stock 감소
+     * */
+    public void removeStock(int quantity) {
+    	int restStock  = this.stockQuantity - quantity;
+    	if(restStock<0) {
+    		throw new NotEnoughStockException("need more stock");
+    	}
+    	this.stockQuantity = restStock;
+    }
 
 }
