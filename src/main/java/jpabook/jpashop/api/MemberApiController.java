63dbe3215694650a4d60cb4da8c9a18ce243b78f
WILL <dndlf333@naver.com>
2023-07-06 00:02:42 +0900

    [기능 추가] 회원수정 API 기능 추가
     -name을 받아서 이름 업데이트 해주는 기능 추가

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,12 +3,15 @@ package jpabook.jpashop.api;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -33,17 +36,37 @@ public class MemberApiController {
 		return new CreateMemberResponse(id);
 	}
 
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+			@RequestBody @Valid UpdateMemberRequest updateMemberRequest) {
+		memberService.update(id, updateMemberRequest.getName());
+		Member findMember = memberService.findOne(id);
+		return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+	}
+
 	@Data
+	@AllArgsConstructor
 	static class CreateMemberResponse {
 		private Long id;
 
-		public CreateMemberResponse(Long id) {
-			this.id = id;
-		}
 	}
 
 	@Data
 	static class CreateMemberRequest {
+		@NotEmpty
+		private String name;
+
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse {
+		private Long id;
+		private String name;
+	}
+
+	@Data
+	static class UpdateMemberRequest {
 		@NotEmpty
 		private String name;
 
