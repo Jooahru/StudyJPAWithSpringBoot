f49d6c9860af7aead9ead694ff1903d966c090e3
WILL <dndlf333@naver.com>
2023-07-10 23:40:27 +0900

    [기능 개선] 간단한 주문 조회 : Entity를 DTO로 변환
     -아직 n+1 문제 존재 => 페치조인으로 해결 예정

--- src/main/java/jpabook/jpashop/api/OrderSimpeApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpeApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 /*
@@ -22,7 +27,7 @@ public class OrderSimpeApiController {
 
 	private final OrderRepository orderRepository;
 
-	@GetMapping("/api/v1/simpe-orders")
+	@GetMapping("/api/v1/simple-orders")
 	public List<Order> ordersV1() {
 		List<Order> all = orderRepository.findAllByString(new OrderSearch());
 		for (Order order : all) { // lazy 강제초기화
@@ -31,4 +36,30 @@ public class OrderSimpeApiController {
 		}
 		return all;
 	}
+
+	@GetMapping("api/v2/simple-orders")
+	public List<SimpleOrderDto> orderV2() {
+		List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+		List<SimpleOrderDto> result = orders.stream().map(o -> new SimpleOrderDto(o)).collect(Collectors.toList());
+		return result;
+	}
+
+	@Data
+	static class SimpleOrderDto {
+		private Long orderId;
+		private String name;
+		private LocalDateTime orderDate;
+		private OrderStatus orderStatus;
+		private Address address;
+
+		public SimpleOrderDto(Order order) {
+			this.orderId = order.getId();
+			this.name = order.getMember().getName();
+			this.orderDate = order.getOrderDate();
+			this.orderStatus = order.getStatus();
+			this.address = order.getDelivery().getAddress();
+		}
+
+	}
+
 }
